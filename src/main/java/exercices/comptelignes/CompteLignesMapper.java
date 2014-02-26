5b557caf93f31b4b6ed52a7ec629499d4e5b99c0
Jean-Francois Im <jfim@jean-francois.im>
2014-02-26 01:22:31 -0500

    Implémenté le mapper et le reducer

--- src/main/java/exercices/comptelignes/CompteLignesMapper.java
+++ src/main/java/exercices/comptelignes/CompteLignesMapper.java
@@ -1,9 +1,22 @@
 package exercices.comptelignes;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 
 /**
  * Le "mapper" du compteur de lignes.
  */
-public class CompteLignesMapper extends Mapper {
+public class CompteLignesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // On ignore ici la valeur. Dans votre code, vous en aurez besoin.
+        String ligne = value.toString();
+
+        // On émet ici une seule valeur, toujours vers la même clef.
+        context.write(new Text("nombreDeLignes"), new IntWritable(1));
+    }
 }
--- src/main/java/exercices/comptelignes/CompteLignesReducer.java
+++ src/main/java/exercices/comptelignes/CompteLignesReducer.java
@@ -1,9 +1,25 @@
 package exercices.comptelignes;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
 
 /**
  * Le "reducer" du compteur de lignes.
  */
-public class CompteLignesReducer extends Reducer {
+public class CompteLignesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int nombreDeLignes = 0;
+
+        // On prend toutes les valeurs et on les additionne
+        for (IntWritable valeur : values) {
+            nombreDeLignes += valeur.get();
+        }
+
+        // Finalement, on écrit la somme
+        context.write(key, new IntWritable(nombreDeLignes));
+    }
 }
